d39acab0f7aafc2a06f02e671a4435f3a9f8ef24
mab-rook <mubarakmahmunalaka@gmail.com>
2022-10-23 21:33:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    let filePath = path.join(__dirname, 'public', 'Home.html')
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      res.writeHead(200, { 'content-Type': 'text/html' })
+      res.end(data);
+    })
+  }
+  if (req.url === '/About.html') {
+    let filePath = path.join(__dirname, 'public', 'About.html')
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      res.writeHead(200, { 'content-Type': 'text/html' })
+      res.end(data);
+    })
+  }
+  if (req.url === '/Contact.html') {
+    let filePath = path.join(__dirname, 'public', 'Contact.html')
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      res.writeHead(200, { 'content-Type': 'text/html' })
+      res.end(data);
+    })
+  }
+})
+
+
+const port = 2000
+server.listen(port, () => {
+  console.log(`server is listening to ${port}`)
+})
